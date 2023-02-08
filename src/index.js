83dc743487157bc00ad1c9cfe318ecb113ba3542
Kola Kolade <kolakolade@gmail.com>
2023-02-08 13:21:52 +0100

    Add font awesome and begin styling main section

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
+import '@fortawesome/fontawesome-free/js/fontawesome';
+import '@fortawesome/fontawesome-free/js/regular';
+import './images/season-2-img.jpeg';
 import './images/the100logo.png';
 import './style.css';
+
