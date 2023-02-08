e90bbaa65d7642a0991932a578461a00887352d6
Kola Kolade <kolakolade@gmail.com>
2023-02-08 22:22:55 +0100

    Add code to retrieve data from provided url in hundred.js

---
+++ src/modules/hundred.js
@@ -0,0 +1,16 @@
+class TheHundred {
+  constructor(url) {
+    this.url = url;
+  }
+
+  getData = async (url) => {
+    await fetch(url, {
+      method: 'GET',
+    }).then((response) => response.json)
+      .then((data) => {
+        localStorage.setItem('data', JSON.stringify(data));
+      });
+  }
+}
+
+export default TheHundred;
