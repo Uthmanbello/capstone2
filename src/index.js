dd69ed7fd98552004451ea9704e1708cc89e00b7
Kola Kolade <kolakolade@gmail.com>
2023-02-08 19:18:59 +0100

    working on getting season data

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@ import '@fortawesome/fontawesome-free/js/fontawesome';
 import '@fortawesome/fontawesome-free/js/regular';
 import './images/season-2-img.jpeg';
 import './images/the100logo.png';
+import TheHundred from './modules/hundred';
 import './style.css';
 
+TheHundred.getData();
