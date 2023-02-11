1c24bd2820b0cfbbe34629656f7e9d9f279d24b7
Uthmanbello <uthmanbello88@gmail.com>
2023-02-11 13:05:29 +0100

    Fix errors using linters

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import '@fortawesome/fontawesome-free/js/regular.js';
 import './images/season-2-img.jpeg';
 import './images/the100logo.png';
 import commentEvent from './modules/commentEvent.js';
-import CommentPopup from './modules/commentPopup.js';
 import Hundred from './modules/hundred.js';
 import resEvent from './modules/reservationEvent.js';
 import View from './modules/views.js';
@@ -13,8 +12,6 @@ import './style.css';
 
 // Initialize
 const url = 'https://api.tvmaze.com/shows/6/seasons';
-const involvementBaseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
-const addLikesURL = `${involvementBaseURL}apps/Ah7jYcR1vh7ExHgt7yOD`;
 
 const show = new Hundred(url);
 show.getData(url);
